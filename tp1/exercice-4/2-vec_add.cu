0b8eab109d6396cd67615f2f6d32685920fd3dc9
Armleth <armand.thibaudon@epita.fr>
2025-10-30 16:50:32 +0100

    feat(tp1): exo4 part 2

--- tp1/exercice-4/2-vec_add.cu
+++ tp1/exercice-4/2-vec_add.cu
@@ -1,60 +1,70 @@
 #include <iostream>
 #include <math.h>
 
-#define cudaCheckError() {                                                                       \
-  cudaError_t e=cudaGetLastError();                                                        \
-  if(e!=cudaSuccess) {                                                                     \
-      printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));        \
-      exit(EXIT_FAILURE);                                                                  \
-  }                                                                                        \
-}
+#define cudaCheckError()                                                       \
+    {                                                                          \
+        cudaError_t e = cudaGetLastError();                                    \
+        if (e != cudaSuccess)                                                  \
+        {                                                                      \
+            printf("Cuda failure %s:%d: '%s'\n", __FILE__, __LINE__,           \
+                   cudaGetErrorString(e));                                     \
+            exit(EXIT_FAILURE);                                                \
+        }                                                                      \
+    }
 
 // CUDA kernel to add elements of two arrays
-__global__ void add(float *x, float *y)
+__global__ void add(float *x, float *y, int N)
 {
-  //@@ add a single element of x and y and store the result in y
+    //@@ add a single element of x and y and store the result in y
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < N)
+        y[i] = x[i] + y[i];
 }
- 
+
 int main(void)
 {
-  int N = 1<<20;  // 1M elements
-  float *x, *y;
- 
-  // Allocate Unified Memory -- accessible from CPU or GPU
-  cudaMallocManaged(&x, N*sizeof(float));
-  cudaMallocManaged(&y, N*sizeof(float));
- 
-  // initialize x and y arrays on the host
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
- 
-  // Launch kernel on 1M elements on the GPU
-  int blockSize = 256;
-  int numBlocks = (N + blockSize - 1) / blockSize;
-  add<<<numBlocks, blockSize>>>(x, y);
- 
-  // Wait for GPU to finish before accessing on host
-  cudaDeviceSynchronize();
-  cudaCheckError();
- 
-  // Check for errors (all values should be 3.0f)
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = fmax(maxError, fabs(y[i]-3.0f));
-  std::cout << "Max error: " << maxError << std::endl;
-  
-  // Free memory
-  cudaFree(x);
-  cudaFree(y);
-  cudaCheckError();
+    int N = 1 << 20; // 1M elements
+    float *x, *y;
 
-  if (maxError < 0.000001f) {
-    printf("Test completed successfully.\n");
-    return 0;
-  } else {
-    printf("WARNING there were some errors.\n");
-    return 1;
-  }
+    // Allocate Unified Memory -- accessible from CPU or GPU
+    cudaMallocManaged(&x, N * sizeof(float));
+    cudaMallocManaged(&y, N * sizeof(float));
+
+    // initialize x and y arrays on the host
+    for (int i = 0; i < N; i++)
+    {
+        x[i] = 1.0f;
+        y[i] = 2.0f;
+    }
+
+    // Launch kernel on 1M elements on the GPU
+    int blockSize = 256;
+    int numBlocks = (N + blockSize - 1) / blockSize;
+    add<<<numBlocks, blockSize>>>(x, y, N);
+
+    // Wait for GPU to finish before accessing on host
+    cudaDeviceSynchronize();
+    cudaCheckError();
+
+    // Check for errors (all values should be 3.0f)
+    float maxError = 0.0f;
+    for (int i = 0; i < N; i++)
+        maxError = fmax(maxError, fabs(y[i] - 3.0f));
+    std::cout << "Max error: " << maxError << std::endl;
+
+    // Free memory
+    cudaFree(x);
+    cudaFree(y);
+    cudaCheckError();
+
+    if (maxError < 0.000001f)
+    {
+        printf("Test completed successfully.\n");
+        return 0;
+    }
+    else
+    {
+        printf("WARNING there were some errors.\n");
+        return 1;
+    }
 }
